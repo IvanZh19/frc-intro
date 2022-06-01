4fa507b6e12fb034436bff49fb864ab52cd2717e
IvanZh19 <ivanzheng930@gmail.com>
2022-06-01 14:58:46 +0000

    added timer to high low goal commands

--- src/main/java/frc/robot/commands/ShootLowGoal.java
+++ src/main/java/frc/robot/commands/ShootLowGoal.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.turret.Turret;
+import java.lang.Thread;
 
 public class ShootLowGoal extends Commandbase {
 
@@ -20,5 +21,7 @@ public class ShootLowGoal extends Commandbase {
     public void execute(){
         turret.setAngle(5);
         turret.shootBall(1.0);
+        Thread.sleep(1000);
+        turret.stop();
     }
 }
